b6d6449a00e8fa6a375048b8b4b2af1dcb271369
Dmytro.R <dmytror@reddotsquare.com>
2016-11-27 22:46:02 +0200

    Read image from phone and try to parse barcode; some code for backporting

---
+++ fxBarcodeScanner/GraphicOverlay.fs
@@ -0,0 +1,86 @@
+﻿module BarcodeTrackerFactory
+
+open Android.Content
+open Android.Graphics
+open Android.Util
+open Android.Views
+open Android.Gms.Vision
+open Android.Hardware
+open Android.Util
+
+type CameraSource() =
+    static member CAMERA_FACING_FRONT = CameraFacing.Front
+    static member CAMERA_FACING_BACK = CameraFacing.Back
+
+module GraphicOverlay =
+    let WIDTH_SCALE_FACTOR = 1.0f
+    let HEIGHT_SCALE_FACTOR = 1.0f
+    let FACING = CameraSource.CAMERA_FACING_BACK
+
+    type Graphic() = // implement comparable
+        abstract member Draw: Canvas -> unit
+        default this.Draw canvas = ()
+
+        member this.ScaleX horizontal = horizontal * WIDTH_SCALE_FACTOR
+        member this.ScaleY vertical = vertical * HEIGHT_SCALE_FACTOR
+        member this.TranslateX facing overlayWidth x = 
+            if facing = CameraSource.CAMERA_FACING_FRONT then 
+                overlayWidth - this.ScaleX(x)
+            else
+                this.ScaleX(x)
+        member this.TranslateY y = this.ScaleY(y)
+    
+    // TODO: https://github.com/googlesamples/android-vision/blob/master/visionSamples/barcode-reader/app/src/main/java/com/google/android/gms/samples/vision/barcodereader/ui/camera/GraphicOverlay.java
+    type T<'a when 'a :> Graphic and 'a: comparison>(context: Context, attributeSet: IAttributeSet) = 
+        inherit View(context, attributeSet)
+
+        let mutable _graphics: Set<'a> = Set.empty
+
+        member this.GetWidth() = 5.0f // from View subclass
+
+        member this.Add graphic =
+            _graphics <- Set.empty.Add graphic
+            this.PostInvalidate()
+
+        member this.Remove graphic =
+            _graphics <- _graphics.Remove(graphic)
+            this.PostInvalidate()
+
+        member this.Clear = 
+            _graphics <- Set.empty
+            this.PostInvalidate()
+
+        member this.GetGraphics() = Set.toList _graphics
+
+//
+//    /**
+//     * Sets the camera attributes for size and facing direction, which informs how to transform
+//     * image coordinates later.
+//     */
+//    public void setCameraInfo(int previewWidth, int previewHeight, int facing) {
+//        synchronized (mLock) {
+//            mPreviewWidth = previewWidth;
+//            mPreviewHeight = previewHeight;
+//            mFacing = facing;
+//        }
+//        postInvalidate();
+//    }
+//
+//    /**
+//     * Draws the overlay with its associated graphic objects.
+//     */
+//    @Override
+//    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+//
+//        synchronized (mLock) {
+//            if ((mPreviewWidth != 0) && (mPreviewHeight != 0)) {
+//                mWidthScaleFactor = (float) canvas.getWidth() / (float) mPreviewWidth;
+//                mHeightScaleFactor = (float) canvas.getHeight() / (float) mPreviewHeight;
+//            }
+//
+//            for (Graphic graphic : mGraphics) {
+//                graphic.draw(canvas);
+//            }
+//        }
+//    }
--- fxBarcodeScanner/MainActivity.fs
+++ fxBarcodeScanner/MainActivity.fs
@@ -16,30 +16,61 @@ open Android.Gms.Vision
 type MainActivity () =
     inherit Activity ()
 
+    [<Literal>]
+    let REQUEST_CODE = 1
+
+    let mutable _imageView: Option<ImageView> = None
+    let mutable _textView: Option<TextView> = None
+    let mutable _button: Option<Button> = None
+
+    let mutable _detector: Option<BarcodeDetector> = None
+
+    let testBitmap = BitmapFactory.DecodeResource(Application.Context.Resources, Resource_Drawable.Puppy)
+
+    let getBarcodeRawValue (detector: BarcodeDetector) bitmap = 
+        let frame = (new Frame.Builder()).SetBitmap(bitmap).Build()
+        let detected = detector.Detect(frame)
+        match detected.Size() with
+        | 0 -> "Invalid barcode!"
+        | _ -> (detector.Detect(frame).ValueAt(0) :?> Barcode).RawValue
+
+    member this.OperationalAction (dataIntent: Intent) =
+        let stream = this.ContentResolver.OpenInputStream(dataIntent.Data)
+        let bitmap = BitmapFactory.DecodeStream(stream);
+
+        _imageView |> Option.iter (fun i -> i.SetImageBitmap(bitmap))
+
+        let raw = getBarcodeRawValue _detector.Value bitmap
+        _textView |> Option.iter (fun t -> t.Text <- sprintf "From activity: %A" raw)
+
+    member this.CameraIntent =
+        let intent = (new Intent()).SetType("image/*").SetAction(Intent.ActionGetContent).AddCategory(Intent.CategoryOpenable);
+        this.StartActivityForResult(intent, REQUEST_CODE);
+
     override this.OnCreate (bundle) =
         base.OnCreate (bundle)
         this.SetContentView (Resource_Layout.Main)
 
-        let appContext = Application.Context
-        let imageView = this.FindViewById<ImageView>(Resource_Id.ImageView)
-        let textView = this.FindViewById<TextView>(Resource_Id.TextView) 
+        _imageView <- this.FindViewById<ImageView>(Resource_Id.ImageView) |> Some
+        _textView <- this.FindViewById<TextView>(Resource_Id.TextView) |> Some
+        _button <- this.FindViewById<Button>(Resource_Id.MyButton) |> Some
 
-        let bitmap = BitmapFactory.DecodeResource(appContext.Resources, Resource_Drawable.Puppy)
-        imageView.SetImageBitmap(bitmap)
+        _imageView |> Option.iter (fun b -> b.SetImageBitmap(testBitmap))
 
-        let detector = (new BarcodeDetector.Builder(appContext))
-                                               .SetBarcodeFormats(BarcodeFormat.QrCode)
-                                               .Build()
+        let supportedBarcodes = BarcodeFormat.QrCode ||| BarcodeFormat.UpcA ||| BarcodeFormat.UpcE
+        _detector <- (new BarcodeDetector.Builder(Application.Context))
+                                               .SetBarcodeFormats(supportedBarcodes)
+                                               .Build() |> Some
+        let detector = _detector.Value
 
-        let button = this.FindViewById<Button>(Resource_Id.MyButton)
-        button.Click.Add (fun args -> 
+        _button |> Option.iter (fun b -> b.Click.Add (fun args -> 
             if not detector.IsOperational then
-                textView.Text <- "Not operational"
+                _textView |> Option.iter (fun t -> t.Text <- "Not operational")
             else
-                textView.Text <- "Operational"
-
-                let frame = (new Frame.Builder()).SetBitmap(bitmap).Build()
-                let barcode = detector.Detect(frame).ValueAt(0) :?> Barcode 
-
-                textView.Text <- barcode.RawValue
-        )
+                _textView |> Option.iter (fun t -> t.Text <- getBarcodeRawValue detector testBitmap; this.CameraIntent)
+        ))
+    
+    override this.OnActivityResult (requestCode: int, resultCode: Result, data: Intent) =
+        match (requestCode, resultCode) with
+        | (REQUEST_CODE, Result.Ok) -> this.OperationalAction data
+        | _ -> ()
